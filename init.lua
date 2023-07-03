8ef4f5731fbe6e12732076871fb8491c10ee054b
SamMoosavi <hi.sammoosavi@gmail.com>
2023-07-03 19:10:17 +0330

    feat(init) get and set the domacsvim runtimepath.

--- init.lua
+++ init.lua
@@ -1,1 +1,3 @@
-print("Hello World!")
+-- get and set the domacsvim runtimepath
+_G.dvim_runtime_dir = vim.env.DVIM_RUNTIME_DIR or debug.getinfo(1, "S").source:sub(2):match("(.*[/\\])"):sub(1, -2)
+vim.opt.rtp:prepend(dvim_runtime_dir)
