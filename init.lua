ce872ac0305df482505cb2e9a5da3a974292ebd1
SamMoosavi <hi.sammoosavi@gmail.com>
2023-07-03 20:01:34 +0330

    feat(init) import bootstrap to setup runtimepath and etc ... .

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 -- get and set the domacsvim runtimepath
 _G.dvim_runtime_dir = vim.env.DVIM_RUNTIME_DIR or debug.getinfo(1, "S").source:sub(2):match("(.*[/\\])"):sub(1, -2)
-vim.opt.rtp:prepend(dvim_runtime_dir)
+vim.opt.rtp:append(dvim_runtime_dir)
+-- import bootstrap to setup runtimepath and etc ...
+require("bootstrap").init()
