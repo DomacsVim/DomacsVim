4c882d1945ca5c087c35e161f76c5828c9bdb0b3
SamMoosavi <hi.sammoosavi@gmail.com>
2023-07-07 19:01:18 +0330

    feat(whichkey) update.

--- lua/core/whichkey.lua
+++ lua/core/whichkey.lua
@@ -1,37 +1,39 @@
 dvim.core.whichkey = {
-  icons = {
-    breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-    separator = "  ", -- symbol used between a key and it's label
-    group = "", -- symbol prepended to a group
-  },
-  plugins = {
-    marks = false, -- shows a list of your marks on ' and `
-    registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-    presets = {
-      operators = false, -- adds help for operators like d, y, ...
-      motions = false, -- adds help for motions
-      text_objects = false, -- help for text objects triggered after entering an operator
-      windows = false, -- default bindings on <c-w>
-      nav = false, -- misc bindings to work with windows
-      z = false, -- bindings for folds, spelling and others prefixed with z
-      g = false, -- bindings for prefixed with g
+  setup = {
+    icons = {
+      breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+      separator = "  ", -- symbol used between a key and it's label
+      group = "", -- symbol prepended to a group
     },
-  },
-  popup_mappings = {
-    scroll_down = "<c-d>", -- binding to scroll down inside the popup
-    scroll_up = "<c-u>", -- binding to scroll up inside the popup
-  },
-  window = {
-    border = "none", -- none/single/double/shadow
-  },
-  layout = {
-    spacing = 6, -- spacing between columns
-  },
-  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " },
-  triggers_blacklist = {
-    -- list of mode / prefixes that should never be hooked by WhichKey
-    i = { "j", "k" },
-    v = { "j", "k" },
+    plugins = {
+      marks = false, -- shows a list of your marks on ' and `
+      registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+      presets = {
+        operators = false, -- adds help for operators like d, y, ...
+        motions = false, -- adds help for motions
+        text_objects = false, -- help for text objects triggered after entering an operator
+        windows = false, -- default bindings on <c-w>
+        nav = false, -- misc bindings to work with windows
+        z = false, -- bindings for folds, spelling and others prefixed with z
+        g = false, -- bindings for prefixed with g
+      },
+    },
+    popup_mappings = {
+      scroll_down = "<c-d>", -- binding to scroll down inside the popup
+      scroll_up = "<c-u>", -- binding to scroll up inside the popup
+    },
+    window = {
+      border = "none", -- none/single/double/shadow
+    },
+    layout = {
+      spacing = 6, -- spacing between columns
+    },
+    hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " },
+    triggers_blacklist = {
+      -- list of mode / prefixes that should never be hooked by WhichKey
+      i = { "j", "k" },
+      v = { "j", "k" },
+    }
   },
   opts = {
     mode = "n", -- NORMAL mode
