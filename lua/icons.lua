50290eadeea6c9c8deeccfb0333ae3bcc2e6a540
SamMoosavi <hi.sammoosavi@gmail.com>
2023-07-07 17:13:28 +0330

    feat(icons) add dev icons.

---
+++ lua/icons.lua
@@ -0,0 +1,169 @@
+local M = {}
+
+local base18 = require("base18").get_theme_table().base18
+
+M.devicons = {
+  c = {
+    icon = "",
+    name = "c",
+    color = base18.base0E
+  },
+
+  css = {
+    icon = "",
+    name = "css",
+    color = base18.base0E
+  },
+
+  deb = {
+    icon = "",
+    name = "deb",
+    color = base18.base10
+  },
+
+  Dockerfile = {
+    icon = "",
+    name = "Dockerfile",
+    color = base18.base0E
+  },
+
+  html = {
+    icon = "",
+    name = "html",
+    color = base18.base10
+  },
+
+  jpeg = {
+    icon = "󰉏",
+    name = "jpeg",
+    color = base18.base0A
+  },
+
+  jpg = {
+    icon = "󰉏",
+    name = "jpg",
+    color = base18.base0A
+  },
+
+  js = {
+    icon = "󰌞",
+    name = "js",
+    color = base18.base0B
+  },
+
+  kt = {
+    icon = "󱈙",
+    name = "kt",
+    color = base18.base0F
+  },
+
+  lock = {
+    icon = "󰌾",
+    name = "lock",
+    color = base18.base10
+  },
+
+  lua = {
+    icon = "",
+    name = "lua",
+    color = base18.base0E
+  },
+
+  mp3 = {
+    icon = "󰎆",
+    name = "mp3",
+    color = base18.base0F
+  },
+
+  mp4 = {
+    icon = "",
+    name = "mp4",
+    color = base18.base0F
+  },
+
+  out = {
+    icon = "",
+    name = "out",
+    color = base18.base0C
+  },
+
+  png = {
+    icon = "󰉏",
+    name = "png",
+    color = base18.base0A
+  },
+
+  py = {
+    icon = "",
+    name = "py",
+    color = base18.base0B
+  },
+
+  ["robots.txt"] = {
+    icon = "󰚩",
+    name = "robots",
+    color = base18.base0G
+  },
+
+  toml = {
+    icon = "",
+    name = "toml",
+    color = base18.base0E
+  },
+
+  ts = {
+    icon = "󰛦",
+    name = "ts",
+    color = base18.base0B
+  },
+
+  ttf = {
+    icon = "",
+    name = "TrueTypeFont",
+    color = base18.base0D
+  },
+
+  rb = {
+    icon = "",
+    name = "rb",
+    color = base18.base10
+  },
+
+  rpm = {
+    icon = "",
+    name = "rpm",
+    color = base18.base10
+  },
+
+  vue = {
+    icon = "󰡄",
+    name = "vue",
+    color = base18.base0C
+  },
+
+  woff = {
+    icon = "",
+    name = "WebOpenFontFormat",
+    color = base18.base0D
+  },
+
+  woff2 = {
+    icon = "",
+    name = "WebOpenFontFormat2",
+    color = base18.base0D
+  },
+
+  xz = {
+    icon = "",
+    name = "xz",
+    color = base18.base0E
+  },
+
+  zip = {
+    icon = "",
+    name = "zip",
+    color = base18.base0E
+  },
+}
+
+return M
