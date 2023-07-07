38dee5d1e9003db9708c7a9d35700f1a037933f3
SamMoosavi <hi.sammoosavi@gmail.com>
2023-07-07 15:04:08 +0330

    feat(plugins) add load lazy method and change structure.

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,3 +1,31 @@
+local function lazy_load(plugin)
+  vim.api.nvim_create_autocmd({ "BufRead", "BufWinEnter", "BufNewFile" }, {
+    group = vim.api.nvim_create_augroup("BeLazyOnFileOpen" .. plugin, {}),
+    callback = function()
+      local file = vim.fn.expand "%"
+      local condition = file ~= "NvimTree_1" and file ~= "[lazy]" and file ~= ""
+
+      if condition then
+        vim.api.nvim_del_augroup_by_name("BeLazyOnFileOpen" .. plugin)
+
+        -- dont defer for treesitter as it will show slow highlighting
+        -- This deferring only happens only when we do "nvim filename"
+        if plugin ~= "nvim-treesitter" then
+          vim.schedule(function()
+            require("lazy").load { plugins = plugin }
+
+            if plugin == "nvim-lspconfig" then
+              vim.cmd "silent! do FileType"
+            end
+          end, 0)
+        else
+          require("lazy").load { plugins = plugin }
+        end
+      end
+    end,
+  })
+end
+
 return {
   {
     "https://gitlab.com/domacsvim/base18",
@@ -11,41 +39,48 @@ return {
     "https://github.com/nvim-tree/nvim-tree.lua",
     cmd = { "NvimTreeToggle", "NvimTreeFocus" },
     event = "User DirOpened",
-    config = function()
-      require("core.nvimtree").setup()
+    opts = function()
+      return require("core.nvimtree")
+    end,
+    config = function(_, opts)
+      require("nvim-tree").setup(opts)
     end,
   },
   {
-    "https://github.com/nvim-treesitter/nvim-treesitter",
-    cmd = {
-      "TSInstall",
-      "TSUninstall",
-      "TSUpdate",
-      "TSUpdateSync",
-      "TSInstallInfo",
-      "TSInstallSync",
-      "TSInstallFromGrammar",
-    },
-    event = "User FileOpened",
-    config = function()
-      local path = vim.fn.stdpath "data" .. "/lazy/nvim-treesitter"
-      vim.opt.rtp:prepend(path) -- treesitter needs to be before nvim's runtime in rtp
-      require("core.treesitter").setup()
+    "nvim-treesitter/nvim-treesitter",
+    init = function()
+      lazy_load("nvim-treesitter")
+    end,
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    opts = function()
+      return require("core.treesitter")
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
     end,
   },
   {
     "https://github.com/windwp/nvim-autopairs",
     event = "InsertEnter",
-    config = function()
-      require("core.autopairs").setup()
+    opts = function()
+      return require("core.autopairs")
+    end,
+    config = function(_, opts)
+      require("nvim-autopairs").setup(opts)
     end,
   },
   {
     "https://github.com/lewis6991/gitsigns.nvim",
     event = "User FileOpened",
     cmd = "Gitsigns",
-    config = function()
-      require("core.gitsigns").setup()
+    init = function()
+      lazy_load("gitsigns.nvim")
+    end,
+    opts = function()
+      return require("core.gitsigns")
+    end,
+    config = function(_, opts)
+      require("gitsigns").setup(opts)
     end,
   },
   {
@@ -64,8 +99,16 @@ return {
   { "https://github.com/nvim-lua/plenary.nvim", cmd = { "PlenaryBustedFile", "PlenaryBustedDirectory" }, lazy = true },
   {
     "https://github.com/nvim-telescope/telescope.nvim",
-    config = function()
-      require("core.telescope").setup()
+    opts = function()
+      return require("core.telescope")
+    end,
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
+
+      pcall(function()
+        telescope.load_extension("projects")
+      end)
     end,
     dependencies = { "telescope-fzf-native.nvim" },
     lazy = true,
