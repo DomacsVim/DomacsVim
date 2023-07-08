630c771ab6f6ecf4ca5ab6402d0a497bbd2387c0
SamMoosavi <hi.sammoosavi@gmail.com>
2023-07-08 12:35:44 +0330

    feat(plugins) add and config indentline plugin.

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -183,4 +183,34 @@ return {
       require("toggleterm").setup(opts)
     end,
   },
+  {
+    "https://github.com/lukas-reineke/indent-blankline.nvim",
+    init = function()
+      lazy_load("indent-blankline.nvim")
+    end,
+    opts = function()
+      dvim.core.indentline = {
+        indentLine_enabled = 1,
+        filetype_exclude = {
+          "help",
+          "terminal",
+          "lazy",
+          "lspinfo",
+          "TelescopePrompt",
+          "TelescopeResults",
+          "mason",
+          "",
+        },
+        buftype_exclude = { "terminal" },
+        show_trailing_blankline_indent = false,
+        show_first_indent_level = false,
+        show_current_context = false,
+        show_current_context_start = false
+      }
+      return dvim.core.indentline
+    end,
+    config = function(_, opts)
+      require("indent_blankline").setup(opts)
+    end,
+  },
 }
