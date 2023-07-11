6c76cf1d1220ed39a7f82dfa8eb2c4f05f4115e4
SamMoosavi <hi.sammoosavi@gmail.com>
2023-07-11 14:56:19 +0330

    feat(utils) add replace_word module.

--- lua/utils.lua
+++ lua/utils.lua
@@ -12,4 +12,14 @@ function M.is_directory(path)
   return stat and stat.type == "directory" or false
 end
 
+function M.replace_word(file, old, new)
+  file = file or dvim_config_dir .. "/init.lua"
+  local instance = io.open(file, "r")
+  local added_pattern = string.gsub(old, "-", "%%-") -- add % before - if exists
+  local new_content = instance:read("*all"):gsub(added_pattern, new)
+  instance = io.open(file, "w")
+  instance:write(new_content)
+  instance:close()
+end
+
 return M
