7bef9120927842e84264c9db88e080fc18999767
SamMoosavi <hi.sammoosavi@gmail.com>
2023-07-19 18:54:46 +0330

    feat(lspconfig)changestructure.

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,57 +1,6 @@
 local M = {}
 
-dvim.core.lspconfig = {
-  keys = {
-    { "gD", vim.lsp.buf.declaration },
-    { "e[", vim.diagnostic.goto_prev },
-    { "e]", vim.diagnostic.goto_next },
-    { "[e", vim.diagnostic.goto_prev },
-    { "]e", vim.diagnostic.goto_next },
-    { "gd", vim.lsp.buf.definition },
-    { "K", vim.lsp.buf.hover },
-    { "gi", vim.lsp.buf.implementation },
-    { "gs", vim.lsp.buf.signature_help },
-    { "gr", vim.lsp.buf.rename },
-    { "gR", vim.lsp.buf.references },
-    { "gca", vim.lsp.buf.code_action },
-    {
-      "F",
-      function()
-        vim.lsp.buf.format({ async = true })
-      end,
-    },
-  },
-  diagnostics = {
-    setup = {
-      virtual_text = {
-        enable = true,
-        prefix = " ",
-        source = "always",
-      },
-      update_in_insert = false,
-      underline = false,
-      severity_sort = true,
-      float = {
-        focusable = true,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-      },
-    },
-    signs = {
-      { name = "DiagnosticSignError", text = " " },
-      { name = "DiagnosticSignWarn", text = " " },
-      { name = "DiagnosticSignHint", text = "" },
-      { name = "DiagnosticSignInfo", text = " " },
-    },
-  },
-  installer = {
-    ensure_installed = { "lua_ls" },
-    automatic_installation = true,
-  },
-}
+require("lsp.defaults")
 
 function M.capabilities()
   local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
@@ -60,12 +9,21 @@ function M.capabilities()
   end
 
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
-  capabilities.textDocument.completion.completionItem.resolveSupport = {
-    properties = {
-      "documentation",
-      "detail",
-      "additionalTextEdits",
+
+  capabilities.textDocument.completion.completionItem = {
+    snippetSupport = true,
+    preselectSupport = true,
+    insertReplaceSupport = true,
+    labelDetailsSupport = true,
+    deprecatedSupport = true,
+    commitCharactersSupport = true,
+    tagSupport = { valueSet = { 1 } },
+    resolveSupport = {
+      properties = {
+        "documentation",
+        "detail",
+        "additionalTextEdits",
+      },
     },
   }
 
@@ -75,26 +33,37 @@ end
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    for k, v in pairs(dvim.core.lspconfig.buffer_options) do
+      vim.api.nvim_buf_set_option(ev.buf, k, v)
+    end
     local bufopts = { noremap = true, silent = true, buffer = ev.buf }
     for _, item in ipairs(dvim.core.lspconfig.keys) do
       vim.keymap.set("n", item[1], item[2], bufopts)
     end
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, dvim.core.lspconfig.diagnostics.setup.float)
+    vim.lsp.handlers["textDocument/signatureHelp"] =
+      vim.lsp.with(vim.lsp.handlers.signature_help, dvim.core.lspconfig.diagnostics.setup.float)
   end,
 })
 
 function M.setup()
+  local lsp_status_ok, _ = pcall(require, "lspconfig")
+  if not lsp_status_ok then
+    return
+  end
+
   for _, sign in ipairs(dvim.core.lspconfig.diagnostics.signs) do
     vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
   end
 
+  pcall(function()
+    require("nlspsettings").setup(dvim.core.lspconfig.nlsp_settings.setup)
+  end)
+
   vim.diagnostic.config(dvim.core.lspconfig.diagnostics.setup)
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, dvim.core.lspconfig.diagnostics.setup.float)
-  vim.lsp.handlers["textDocument/signatureHelp"] =
-    vim.lsp.with(vim.lsp.handlers.signature_help, dvim.core.lspconfig.diagnostics.setup.float)
 
   require("mason-lspconfig").setup(dvim.core.lspconfig.installer)
+
   require("lsp.manager")
 end
 
