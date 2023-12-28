49f5b7566639dee00f9b86f94e48dc0d9f4e6f74
SamMoosavi <hi.sammoosavi@gmail.com>
2023-12-28 19:04:41 +0330

    feat(plugins) update.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"https://gitlab.com/domacsvim/themer",
 		config = function()
-			local themer = require("themer")
+			local themer = require 'themer'
 			themer.apply_theme()
 		end,
 		lazy = false
