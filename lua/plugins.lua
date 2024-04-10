d8c44854493d0282a5a00f6ee18de86e2a3d8dbb
SamMoosavi <hi.sammoosavi@gmail.com>
2024-04-10 21:14:46 +0330

    feat(lua/plugins): added nvimtree plugin.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,19 @@
 return {
-	{
-		"https://gitlab.com/domacsvim/themer",
-		config = function()
-			local themer = require 'themer'
-			themer.apply_theme()
-		end,
-		lazy = false
-	}
+  {
+    "https://gitlab.com/domacsvim/themer",
+    config = function()
+      local themer = require 'themer'
+      themer.apply_theme()
+    end,
+    lazy = false
+  },
+  {
+    "https://github.com/nvim-tree/nvim-tree.lua",
+    config = function()
+      require("core.nvimtree").setup()
+    end,
+    enabled = dvim.core.nvimtree.active,
+    cmd = { "NvimTreeToggle", "NvimTreeOpen", "NvimTreeFocus", "NvimTreeFindFileToggle" },
+    event = "User DirOpened",
+  },
 }
