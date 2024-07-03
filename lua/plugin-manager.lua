3af729d76aaa28c7ad008ec118c4cc2fdb6c13a1
SamMoosavi <hi.sammoosavi@gmail.com>
2024-07-03 23:36:41 +0330

    feat(plugin-manager.lua): change border.

--- lua/plugin-manager.lua
+++ lua/plugin-manager.lua
@@ -47,7 +47,7 @@ dvim.lazy = {
 		size = { width = 0.7, height = 0.7 },
 		wrap = true, -- wrap the lines in the ui
 		-- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
-		border = "none",
+		border = "rounded",
 		title = "Lazy", ---@type string only works when border is not "none"
 		title_pos = "center", ---@type "center" | "left" | "right"
 		-- Show pills on top of the Lazy window
